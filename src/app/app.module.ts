c1a2e1e3267557c1fa23a32990fecc8b235a927c
Teemu Nasanen <teemu.nasanen@gmail.com>
2022-02-03 09:39:48 +0200

    feat(routing and pages): created pages and added routing for landing, catalogue and trainer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TrainerListComponent } from './trainer/trainer-list/trainer-list.component';
+import { CataloguePage } from './catalogue/catalogue.page';
+import { LandingPage } from './landing/landing.page';
+import { TrainerListComponent } from './trainer-list/trainer-list.component';
+import { TrainerPage } from './trainer/trainer.page';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TrainerListComponent
+    TrainerListComponent,
+    TrainerPage,
+    LandingPage,
+    CataloguePage
   ],
   imports: [
     BrowserModule,
