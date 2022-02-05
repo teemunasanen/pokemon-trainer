af6a4298923e4425563f5e5e359827f19f7f1162
Nico Behnen <Azruim@live.fi>
2022-02-05 15:45:52 +0200

    feat: refactored catalogue service.

--- src/app/services/catalogue.service.ts
+++ src/app/services/catalogue.service.ts
@@ -1,38 +1,34 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map, mergeMap } from "rxjs";
 import { PokemonShort, PokemonListObject, PokemonRaw } from "../models/pokemon-list.model";
 
 @Injectable({
     providedIn: 'root'
 })
 export class CatalogueService {
-    private _catalogue: PokemonShort[] = []
+    private _catalogue: PokemonShort[] = [];
 
     constructor(private readonly http: HttpClient) {
     }
 
     public fetchCatalogue(numOfPokemons: number): void {
         this.http.get<PokemonListObject>('https://pokeapi.co/api/v2/pokemon?limit=' + numOfPokemons)
+            .pipe(mergeMap(response => response.results.map(results => this.http.get<PokemonRaw>(results.url))))
             .subscribe({
-                next: (catalogueObject) => {
-                    for (let i = 0; i < catalogueObject.results.length; i++) {
-                        this.fetchPokemon(catalogueObject.results[i].url);
-                    }
-                }
-            })
-    }
-
-    public fetchPokemon(url: string): void {
-        this.http.get<PokemonRaw>(url)
-            .subscribe({
-                next: (pokemon) => {
-                    const newPokemon: PokemonShort = {
-                        name: pokemon.name,
-                        sprite: pokemon.sprites.versions["generation-vii"]["ultra-sun-ultra-moon"].front_default,
-                        type: pokemon.types[0].type.name,
-                        caught: false
-                    };
-                    this._catalogue.push(newPokemon);
+                next: (object) => {
+                    object.subscribe({
+                        next: (result) => {
+                            const newPokemon: PokemonShort = {
+                                name: result.name,
+                                sprite: result.sprites.versions["generation-vii"]["ultra-sun-ultra-moon"].front_default,
+                                type: result.types[0].type.name,
+                                caught: false
+                            };
+                            this._catalogue.push(newPokemon);
+                            sessionStorage.setItem("catalogue", JSON.stringify(this._catalogue));
+                        }
+                    })
                 }
             })
     }
