20eaa43a20fdb538a74b01926665e76bc96de1dc
Teemu Nasanen <teemu.nasanen@gmail.com>
2022-02-08 09:55:59 +0200

    style: added hovering to buttons and class to nav-links

--- src/app/trainer/trainer.page.ts
+++ src/app/trainer/trainer.page.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-trainer',
-    templateUrl: './trainer.page.html'
+    templateUrl: './trainer.page.html',
+    styleUrls: ['./trainer.page.css']
 })
 export class TrainerPage {}
